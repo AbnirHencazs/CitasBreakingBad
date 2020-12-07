e597f18452f696a01e428a64c855ede990be8e22
Brian Sanchez <briansanchezizq@gmail.com>
2020-12-07 16:02:57 -0600

    Solicitud a API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+import styled from '@emotion/styled';
+
+const Contenedor = styled.div`
+  display:flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+
+const Boton = styled.button`
+  
+  background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0F574E 100%);
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+`;
+
+const handleClick = async e => {
+  const req = await fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes')
+  const promesa = await req.json();
+  console.log(promesa[0])
+}
 
 function App() {
   return (
-    <h1>Frases BreakingBad</h1>
+    <Contenedor>
+      <Boton
+        onClick={handleClick}>
+        Obtener frase
+      </Boton>
+    </Contenedor>
   );
 }
 
