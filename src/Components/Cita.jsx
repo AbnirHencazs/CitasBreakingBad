76629057dd90474e7b5534f14552d8def9a4ecea
Brian Sanchez <briansanchezizq@gmail.com>
2020-12-07 16:18:28 -0600

    Agrego estilos a componente Cita

--- src/Components/Cita.jsx
+++ src/Components/Cita.jsx
@@ -1,11 +1,46 @@
 import React, {Fragment} from 'react';
+import styled from '@emotion/styled';
+
+const ContenedorFrase = styled.div`
+    padding: 3rem;
+    border-radius: 1.5rem;
+    background-color: #fff;
+    max-width: 800px;
+    @media (min-width: 992px){
+        margin-top: 10rem;
+    }
+    //Puedes seleccionar los hijos dentro del contenedor
+    h1{
+        font-family: Arial, Helvetica, sans-serif;
+        text-align: center;
+        position: relative;
+        padding-left: 4rem;
+
+        &::before{
+            content: open-quote;
+            font-size: 10rem;
+            color: black;
+            position: absolute;
+            left: -1rem;
+            top: -2rem;
+        }
+    }
+    p{
+        font-family: Verdana, Geneva, Tahoma, sans-serif;
+        font-size: 1.4rem;
+        font-weight:bold;
+        text-align: right;
+        color: #666;
+        margin-top: 2rem;
+    }
+`;
 
 const Cita = ({ cita }) => {
     return(
-        <Fragment>
-            <h1>{cita.author}</h1>
-            <p>-{cita.quote}</p>
-        </Fragment>
+        <ContenedorFrase>
+            <h1>{cita.quote}</h1>
+            <p>-{cita.author}</p>
+        </ContenedorFrase>
     );
 }
 
